ee77c0c6ae5dc1659c03d0d88e6a59e3004afe43
kriangpo <kriangsak@eoss-th.com>
2018-09-28 16:31:55 +0700

    connect db

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,46 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.User;
+
 @RestController
 public class HelloController {
 
-	@RequestMapping("/test")
+	@Autowired
+    JdbcTemplate jdbcTemplate;
+    
+	@RequestMapping("/")
     public String restTest(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Return name= " + name ;
+        return "Hello " + name ;
     }
+	
+	
+	@RequestMapping("/getUser")
+    public List<User> getUser(@RequestParam(value="name",required=false) String name) {
+		
+		String sql = "SELECT * FROM users WHERE firstname like ?";
+        List<Object> params = new ArrayList<Object>();
+        params.add("%"+name+"%");
+        
+        Object arr [] = params.toArray(new Object[params.size()]);
+        
+		RowMapper<User> rowMapper = new BeanPropertyRowMapper<User>(User.class);
+		
+		List<User> article = jdbcTemplate.query(sql, rowMapper, arr);
+    	
+        return article;
+    }
+	
 	
 }
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class User {
+
+	private int id;
+	private String username;
+	private String firstname;
+	private String lastname;
+	private Date lastlogin;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Date getLastlogin() {
+		return lastlogin;
+	}
+	public void setLastlogin(Date lastlogin) {
+		this.lastlogin = lastlogin;
+	}
+	
+	
+}
